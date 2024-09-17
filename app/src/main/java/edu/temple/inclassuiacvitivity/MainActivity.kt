6551aa4ed3eb4f44eecffedfd31dd95d48ee230c
Mitchell Fishman <tuq42483@temple.edu>
2024-09-17 17:21:55 -0400

    Completed Step 4: Change TextView's text size to number selected in the Spinner.

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +19,23 @@ class MainActivity : AppCompatActivity() {
         /* TODO Step 3: Create adapter to display items from array in Spinner */
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, numberArray)
-        spinner = findViewById<Spinner>(R.id.spinner)
         spinner.adapter = adapter
 
 
         // TODO Step 4: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                view?.run {
+                    val selectedNumber = numberArray[position]
+                    displayTextView.setTextSize(selectedNumber.toFloat())
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+        }
 
     }
 }
